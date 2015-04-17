d7d6730fc469437c52a38a33984880ae7bc82c84
Scott Burns <scott@lentigo.net>
2015-04-16 22:02:09 -0400

    unzip data file into data directory after it downloads (flatten subdirectories).

--- run_analysis.R
+++ run_analysis.R
@@ -2,12 +2,13 @@
 dataURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 localDataFile <- "./data/HAR Dataset.zip"
 
-# if not already existent, create directory to hold data files and download them to it
+# if not already existent, create directory to hold data files; download the zip file
+# and unzip the files
 if(!file.exists("data")) {
   dir.create("data")
 }
 
 if(!file.exists(localDataFile)) {
   download.file(dataURL, destfile=localDataFile, method="curl")
+  unzip(localDataFile, overwrite=FALSE, junkpaths=TRUE, exdir="./data")
 }
-
