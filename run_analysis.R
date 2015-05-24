f47db30fd198d7562f1581ba11587793fcd025e3
Scott Burns <scott@lentigo.net>
2015-05-24 10:23:13 -0400

    Removed Unused Function
    Remove un-used get_mean_sd function

--- run_analysis.R
+++ run_analysis.R
@@ -16,7 +16,7 @@ if(!file.exists(localDataFile)) {
   unzip(localDataFile, overwrite=FALSE, junkpaths=TRUE, exdir="./data")
 }
 
-## Define functions for reading and combining data files and reporting means
+## Define function for reading and combining data files
 
 # combine_test_training - takes a file name part (the name excluding _test or
 # _training) of a target data type; reads the data from the test and training
@@ -28,23 +28,6 @@ combine_test_training <- function(m_type) {
   train_data <- read.delim(paste0("./data/", m_type, "_train.txt"),
                            header=FALSE, sep="")
   rbind(test_data, train_data)
-}
-
-# get_mean_sd: takes a data.frame with raw data and a label prefix and returns a
-# data.frame containing the mean and standard deviation for each row of
-# measurements; provides descriptive names for the columns based on the label
-# prefix passed
-get_mean_sd <- function(data, label_prefix) {
-  # calculate the mean and standard deviation of each row/measurement
-  Means <- as.numeric(apply(data, c(1), mean))
-  SDs <- as.numeric(apply(data, c(1), sd))
-
-  # combine the mean and sd vectors into a data frame; provide meaningful names
-  # for the columns, and return to the caller
-  means_sds <- data.frame(cbind(Means, SDs))
-  names(means_sds)[1] <- paste0(label_prefix, ".Mean")
-  names(means_sds)[2] <- paste0(label_prefix, ".SD")
-  return(means_sds)
 }
 
 ## Generate the tidy data set
